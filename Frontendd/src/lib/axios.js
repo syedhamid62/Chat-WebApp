7b5578e3526640fb941f06ec546d844903aac34e
syedhamid62 <syedhamid786op2@gmail.com>
2025-04-22 01:30:57 +0530

    changed bd url

--- Frontendd/src/lib/axios.js
+++ Frontendd/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-
+console.log(import.meta.env.VITE_BASE_URL)
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://chat-webapp-xzvs.onrender.com/api",
+  baseURL: `${import.meta.env.VITE_BASE_URL}/api`,
   withCredentials: true,
 });
